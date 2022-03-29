abafb3d0f47a860e280177be75ae763d3d70bc15
[Senanur KOKMAZ] <senakorkmaz06@hotmail.com>
2022-03-29 18:55:38 +0300

    Change db path

--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/senak/Documents/GitHub/Todo-App-Flask/todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/senak/Documents/GitHub/Todo-App-Flask/templates/todo.db'
 db = SQLAlchemy(app)
 
 @app.route('/')
